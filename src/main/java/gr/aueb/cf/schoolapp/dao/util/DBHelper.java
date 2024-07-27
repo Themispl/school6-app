cef6a3f95ee727f765247149e3af0b0c95afd665
Themispl <themis.pilarinos@gmail.com>
2024-07-27 10:32:29 +0300

    Create TeacherDAOTest

--- src/main/java/gr/aueb/cf/schoolapp/dao/util/DBHelper.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/util/DBHelper.java
@@ -14,30 +14,32 @@ public class DBHelper {
     private DBHelper(){
 
     }
-    public static  void eraseData(){
+    public static void eraseData() throws SQLException {
         String sqlFKOff = "SET @@foreign_key_checks = 0";
         String sqlFKOn = "SET @@foreign_key_checks = 1";
         String sqlSelect = "SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_SCHEMA = 'school6db'";
-        ResultSet rs = null ;
+        ResultSet rs = null;
 
-        try{
-            Connection connection = DBUtil.getConnection();
-            PreparedStatement ps1 = connection.prepareStatement(sqlFKOn);
-            PreparedStatement ps2 = connection.prepareStatement(sqlSelect);
+        try (Connection connection = DBUtil.getConnection();
+             PreparedStatement ps1 = connection.prepareStatement(sqlFKOff);
+             PreparedStatement ps2 = connection.prepareStatement(sqlSelect)) {
+
             ps1.executeUpdate();
             rs = ps2.executeQuery();
             List<String> tables = mapRsToList(rs);
-            for(String table : tables){
+
+            for (String table : tables) {
                 connection.prepareStatement("DELETE FROM " + table).executeUpdate();
-                connection.prepareStatement("ALTER FROM " + table + "AUTO_INCREMENT=1").executeUpdate();
+                connection.prepareStatement("ALTER TABLE " + table + " AUTO_INCREMENT=1").executeUpdate();
             }
             connection.prepareStatement(sqlFKOn).executeUpdate();
-
-        }catch (SQLException me){
-            me.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
         }
     }
-    private  static List<String> mapRsToList(ResultSet rs) throws SQLException{
+
+    private static List<String> mapRsToList(ResultSet rs) throws SQLException {
         List<String> tables = new ArrayList<>();
 
         while (rs.next()) {
